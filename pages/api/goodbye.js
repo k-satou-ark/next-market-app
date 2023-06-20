23a47bae56a7f70be0e2d25df735eac2c0da8680
k-satou <k-satou@arkline.co.jp>
2023-06-20 14:51:43 +0900

    変更

--- pages/api/goodbye.js
+++
@@ -1,3 +0,0 @@
-const goodbye = (req, res) => { return res. status( 200). json({ message: "さようなら"}) } export default goodbye
-
-三好アキ. Next.jsでつくるフルスタックアプリ with TypeScript　前編（バックエンド開発） (p.42). Kindle 版. 
--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const hello = (req, res) => {
-  return res.status(200).json({ message: "こんにちは", date: "今日は金曜日です" })
-}
-
-export default hello
